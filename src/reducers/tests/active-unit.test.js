e16173fbb24d26272cff276b8d85e0a15918f9f2
Jared Easterday <easterday@planet.com>
2017-08-26 15:52:54 -0700

    tests

---
+++ src/reducers/tests/active-unit.test.js
@@ -0,0 +1,23 @@
+import reducer from '../activeUnit'; // <- rename that file
+
+describe('accive-unit reducer', () => {
+  it('returns default state', () => {
+    expect(true).toBe(true);
+  });
+});
+
+// const activeUnit = (state = {}, action) => {
+//   switch (action.type) {
+//     case 'SET_ACTIVE_UNIT':
+//       // TODO: check for team ID before
+//       return {
+//         id: action.payload.id,
+//         location: action.payload.location
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export default activeUnit;
