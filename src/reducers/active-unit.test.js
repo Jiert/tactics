70aebb33f8b79c75ab176d8f2e56a9de9cda88b1
Jared Easterday <easterday@planet.com>
2017-08-26 16:56:29 -0700

    cleanup, eslint env jasmine

--- src/reducers/active-unit.test.js
+++ src/reducers/active-unit.test.js
@@ -22,19 +22,3 @@ describe('accive-unit reducer', () => {
     expect(reducer({}, action)).toEqual(expectedState);
   });
 });
-
-// const activeUnit = (state = {}, action) => {
-//   switch (action.type) {
-//     case 'SET_ACTIVE_UNIT':
-//       // TODO: check for team ID before
-//       return {
-//         id: action.payload.id,
-//         location: action.payload.location
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
-
-// export default activeUnit;
