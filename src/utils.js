01d5982f39daaf9c3ba39cb284be39d144918e92
Jared Easterday <easterday@planet.com>
2017-08-25 21:09:44 -0700

    early idea for terrain

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,15 @@ export const distanceMoved = (prevLoc, currentLoc) => {
 
   return Math.max(xDistance, yDistance);
 };
+
+export const chance = () => Math.random() * (10 - 0) + 0 < 1.5;
+
+// This is just an idea
+export const generateTerrain = () => {
+  // this is just for fun, the real terrain will have to be on the server
+  if (this.chance()) {
+    return '🌲';
+  } else if (this.chance()) {
+    return '⛰';
+  }
+};
